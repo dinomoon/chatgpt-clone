e85ff10f2a514418adf02ab17238bafce77ea66c
dinomoon <ansrud45@gmail.com>
2025-01-03 21:59:10 +0900

    Add auth check middleware

---
+++ constants/routes.ts
@@ -0,0 +1,12 @@
+export const BASE_URL = '/';
+
+export const AUTH_ROUTES = {
+  SIGN_IN: '/signin',
+  SIGN_UP: '/signup',
+};
+
+export const CHAT_ROUTES = {
+  CONVERSATIONS: '/c',
+};
+
+export const PUBLIC_ROUTES = [AUTH_ROUTES.SIGN_IN, AUTH_ROUTES.SIGN_UP];

b62f0b1079e6d46e0b112680a996cb3e85979d4a
dinomoon <ansrud45@gmail.com>
2025-01-03 23:13:27 +0900

    Add Header

---
+++ src/store/model.ts
@@ -0,0 +1,17 @@
+import { create } from 'zustand';
+import { createWithEqualityFn } from 'zustand/traditional';
+
+type State = {
+  model: string;
+};
+
+type Action = {
+  updateModel: (model: State['model']) => void;
+};
+
+const useModelStore = createWithEqualityFn<State & Action>((set) => ({
+  model: 'gpt-3.5-turbo',
+  updateModel: (model) => set(() => ({ model })),
+}));
+
+export { useModelStore };
